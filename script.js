7c79428cdd5d5127c0edda1cb8b418b669c1a3b3
Iwantroca <mohitnotiyal@yahoo.com>
2021-05-05 01:37:26 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let computerPick;
+let computerItem;
+
+// Computer Picks a random item
+let computerPlay = () => {
+  computerPick = Math.floor(Math.random() * 3);
+  switch (computerPick) {
+    case 0:
+      computerItem = "Rock";
+      break;
+    case 1:
+      computerItem = "Paper";
+      break;
+    case 2:
+      computerItem = "Scissors";
+      break;
+  }
+  return computerItem;
+};
+computerPlay();
+
+// Human picks an item
+let humanItem;
+let humanPlay = (text) => {
+  humanItem = prompt("Pick an item", "Rock, Paper or Scissors");
+  humanItem = humanItem.toLowerCase();
+  humanItem = humanItem.charAt(0).toUpperCase() + humanItem.slice(1);
+  return humanItem;
+};
+
+let counter = 1;
+let humanScore = 0;
+let computerScore = 0;
+
+// initiates the one round game
+let roundPlay = (computerMove, humanMove) => {
+  if (
+    (computerMove === "Rock" && humanMove === "Paper") ||
+    (computerMove === "Paper" && humanMove === "Scissors") ||
+    (computerMove === "Scissors" && humanMove === "Rock")
+  ) {
+    alert("Human Wins");
+    alert(`Human: ${humanMove} beats the Computer: ${computerMove}`);
+    counter += 1;
+    humanScore += 1;
+  } else if (computerMove === humanMove) {
+    alert("It is a draw");
+    alert(`Both players pick the ${humanMove}`);
+
+    counter += 0;
+  } else {
+    alert("Computer Wins");
+    alert(`Computer: ${computerMove} bests the Human: ${humanMove}`);
+    computerScore += 1;
+
+    counter += 1;
+  }
+};
+
+// tracks the counter and if number of rounds is less than 5 then
+//      keeps the rounds of game going.
+while (counter < 6) {
+  computerPlay();
+  newHumanPlay = humanPlay();
+  roundPlay(computerItem, newHumanPlay);
+}
+
+let result =
+  humanScore > computerScore
+    ? alert(`Human is the real champion ${humanScore}-${computerScore}`)
+    : alert(
+        `Computer beats the hell out of human ${computerScore}-${humanScore}`
+      );
