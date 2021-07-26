03628e2e4199cbe1b260e49d97d1c976deaa50fb
Iwantroca <mohitnotiyal@yahoo.com>
2021-07-26 18:35:02 +0530

    Refactor the script code

--- script.js
+++ script.js
@@ -1,75 +1,49 @@
-let computerPick;
+// computer selects a random item
+// if either of the player have scored 5, stop the game
+// human selects an item
+// iterate through buttons
+// add event listener to class of both icon and button div to get the value of 'weapon'
+// as the button is clicked, also initiates a function to run the round
+//
+// add a score of +1 to winner
+
 let computerItem;
+let playerItem;
 
-// Computer Picks a random item
-let computerPlay = () => {
-  computerPick = Math.floor(Math.random() * 3);
-  switch (computerPick) {
-    case 0:
-      computerItem = "Rock";
-      break;
-    case 1:
-      computerItem = "Paper";
-      break;
-    case 2:
-      computerItem = "Scissors";
-      break;
-  }
+let buttons = document.querySelectorAll("button");
+
+function computerPick() {
+  let selectItem = Math.floor(Math.random() * 3);
+  if (selectItem === 0) computerItem = "rock";
+  else if (selectItem === 1) computerItem = "paper";
+  else if (selectItem === 2) computerItem = "scissors";
   return computerItem;
-};
-computerPlay();
+}
 
-// Human picks an item
-let weapons = document.querySelectorAll(".weapon");
-let humanMove;
-weapons.forEach((weapon) => {
-  weapon.addEventListener("click", () => {
-    humanMove = weapon.classList[0];
-    console.log(humanMove);
-  });
-});
+function playerPick(e) {
+  playerItem = e.target.classList[0];
+  return playerItem;
+}
 
-let counter = 1;
-let humanScore = 0;
-let computerScore = 0;
-
-// initiates the one round game
-let roundPlay = (computerMove, humanMove) => {
+// plays a round of game b/w computer and player
+function roundPlay(computerMove, humanMove) {
   if (
-    (computerMove === "Rock" && humanMove === "Paper") ||
-    (computerMove === "Paper" && humanMove === "Scissors") ||
-    (computerMove === "Scissors" && humanMove === "Rock")
+    (computerMove === "rock" && humanMove === "scissors") ||
+    (computerMove === "paper" && humanMove === "rock") ||
+    (computerMove === "scissors" && humanMove === "paper")
   ) {
-    alert("Human Wins");
-    alert(`Human: ${humanMove} beats the Computer: ${computerMove}`);
-    counter += 1;
-    humanScore += 1;
-  } else if (computerMove === humanMove) {
-    alert("It is a draw");
-    alert(`Both players pick the ${humanMove}`);
-
-    counter += 0;
+    console.log("Computer beats the player");
+  } else if (computerMove == humanMove) {
+    console.log("It is a draw");
   } else {
-    alert("Computer Wins");
-    alert(`Computer: ${computerMove} bests the Human: ${humanMove}`);
-    computerScore += 1;
-
-    counter += 1;
+    console.log("Human beats the computer");
   }
-};
+}
 
-// tracks the counter and if number of rounds is less than 5 then
-//      keeps the rounds of game going.
-
-// while (counter < 6) {
-//   computerPlay();
-//   newHumanPlay = humanPlay();
-//   roundPlay(computerItem, newHumanPlay);
-// }
-
-// let result =
-//   humanScore > computerScore
-//     ? alert(`Human is the real champion ${humanScore}-${computerScore}`)
-//     : alert(
-//         `Computer beats the hell out of human ${computerScore}-${humanScore}`
-//       );
+buttons.forEach((button) => button.addEventListener("click", computerPick));
+buttons.forEach((button) => button.addEventListener("click", playerPick));
+buttons.forEach((button) =>
+  button.addEventListener("click", function () {
+    roundPlay(computerItem, playerItem);
+  })
+);
